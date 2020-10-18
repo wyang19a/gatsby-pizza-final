60713fbd29cecbd6bf51445a926ccb129598a143
wyang19a <wyang19a@gmail.com>
2020-10-18 12:43:24 +0900

    Adds responsiveness

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -25,6 +25,12 @@ const PaginationStyles = styled.div`
       color: var(--grey);
     }
   }
+  @media (max-width: 800px) {
+    .word {
+      display: none;
+      font-size: 1.4rem;
+    }
+  }
 `;
 
 export default function Pagination({
@@ -41,8 +47,12 @@ export default function Pagination({
   const hasPrevPage = prevPage >= 1;
   return (
     <PaginationStyles>
-      <Link disabled={!hasPrevPage} to={`/${base}/${prevPage}`}>
-        ← Prev
+      <Link
+        title="Prev Page"
+        disabled={!hasPrevPage}
+        to={`/${base}/${prevPage}`}
+      >
+        ← <span className="word">Prev</span>
       </Link>
       {Array.from({ length: totalPages }).map((_, i) => (
         <Link
@@ -53,8 +63,12 @@ export default function Pagination({
           {i + 1}
         </Link>
       ))}
-      <Link disabled={!hasNextPage} to={`/${base}/${nextPage}`}>
-        Next →
+      <Link
+        title="Next Page"
+        disabled={!hasNextPage}
+        to={`/${base}/${nextPage}`}
+      >
+        <span className="word">Next</span> →
       </Link>
     </PaginationStyles>
   );
